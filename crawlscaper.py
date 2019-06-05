4be8678158aa7c86447184e27be51953187f1ad8
0xRainy <35612912+0xRainy@users.noreply.github.com>
2019-06-05 10:16:43 -0700

    Wrapped in function and string splitting in to a dict of users and site they are playing on

--- crawlscaper.py
+++ crawlscaper.py
@@ -2,19 +2,48 @@ import time
 from selenium import webdriver
 from bs4 import BeautifulSoup
 
-url = "http://crawl.akrasiac.org:8080/#lobby"
-browser = webdriver.PhantomJS()
-browser.get(url)
-time.sleep(0.05)  # Change this to some sort of 'Wait for response'
-soup = BeautifulSoup(browser.page_source, "lxml")
-browser.quit()
-table = soup.findAll('td', {"class": "username"})
-a = []
-for x in table:
-    x.find_all('a')
-    for y in x:
-        y = str(y)
-        y = y.split('>')[1]
-        y = y.split('<')[0]
-        a.append(''.join(y))
-[print(i) for i in a]
+
+# This works for each individual site #
+
+# url = "http://crawl.berotato.org:8080/#lobby"
+# browser = webdriver.PhantomJS()
+# browser.get(url)
+# time.sleep(2)  # Change this to some sort of 'Wait for response'
+# soup = BeautifulSoup(browser.page_source, "lxml")
+# browser.quit()
+# table = soup.findAll('td', {"class": "username"})
+# a = []
+# for x in table:
+#     x.find_all('a')
+#     for y in x:
+#         y = str(y)
+#         y = y.split('>')[1]
+#         y = y.split('<')[0]
+#         a.append(''.join(y))
+# [print(i) for i in a]
+
+# This grabs from a list of every active player on all crawl sites
+def GrabData_AllCrawlSites():
+    url = "https://crawl.develz.org/watch.htm"
+    browser = webdriver.PhantomJS()
+    browser.get(url)
+    time.sleep(2)
+    soup = BeautifulSoup(browser.page_source, "lxml")
+    browser.quit()
+    table = soup.findChildren('tr')
+    user_dict = {}
+    for child in table:
+        child = child.findNext('a')
+        child = str(child)
+        site = child.split('//')[-1]
+        site = site.split('/')[0]
+        if site == 'crawl.project357.org':
+            user = child.split('h/')[1]
+        else:
+            user = child.split('-')[-1]
+        user = user.split('"')[0]
+        user_dict.update({user: site})
+    return user_dict
+
+
+# print(GrabData_AllCrawlSites())
