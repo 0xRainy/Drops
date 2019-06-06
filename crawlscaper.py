9328347f6ca3307365fb2232c85b196fa370f469
0xRainy <35612912+0xRainy@users.noreply.github.com>
2019-06-05 21:01:19 -0700

    Added support for following players

--- crawlscaper.py
+++ crawlscaper.py
@@ -8,7 +8,6 @@ from pathlib import Path
 from selenium import webdriver
 from bs4 import BeautifulSoup
 
-
 # This works for each individual site #
 
 # url = "http://crawl.berotato.org:8080/#lobby"
@@ -67,6 +66,18 @@ class Leaderboards():
                 leaderfile.write(i + '\n')
 
 
+def add_followers():
+    """ Reads from file followed_players.txt and returns a list.
+        Entries in followed_players.txt must be separated by new lines.
+    """
+    followers_file = Path(FOLLOWED_PLAYERS_FILE)
+    if FOLLOWED_PLAYERS_FILE.is_file():
+        with open(followers_file, "r") as bfile:
+            temp_followed_players = bfile.read()
+    followed_players = (temp_followed_players.rstrip('\n')).splitlines()
+    return followed_players
+
+
 def grabdata_all_crawl_sites():
     """Returns dict of players and site they are using"""
 
@@ -78,7 +89,7 @@ def grabdata_all_crawl_sites():
     soup = BeautifulSoup(browser.page_source, "lxml")
     browser.quit()
     table = soup.findChildren('tr')
-    user_dict = {'test': 'testsite', 'test2': 'testsite2'}
+    user_dict = {'test': 'testsite', 'test2': 'testsite2', 'gammafunk': 'testsite3', 'poop': 'testsite4', 'qooq': 'testsite5'}
     for child in table:
         child = child.findNext('a')
         child = str(child)
@@ -95,7 +106,10 @@ def grabdata_all_crawl_sites():
 
 LEADERBOARDS = Leaderboards('http://crawl.akrasiac.org/scoring/streaks.html',
                             'top_streak.txt')
+FOLLOWED_PLAYERS_FILE = Path('./followed_players.txt')
 LEADERBOARDS_FILE = Path('./' + LEADERBOARDS.filename)
+FOLLOWED_PLAYERS_LIST = add_followers()
+
 if LEADERBOARDS_FILE.is_file():
     print('Leaderbord file found.')
 else:
@@ -105,12 +119,40 @@ else:
 with open(LEADERBOARDS.filename, "r") as afile:
     TEMP_TOP_PLAYERS = afile.read()
 TOP_PLAYERS = (TEMP_TOP_PLAYERS.rstrip('\n')).splitlines()
+
+if FOLLOWED_PLAYERS_FILE.is_file():
+    print('Followed Players file found.')
+    print('Added ' + str(len(FOLLOWED_PLAYERS_LIST)) + ' players to watch list...')
+else:
+    print('No Followed Players file found, create one!')
+    print('No Followed Players added to watch list!')
+
 ACTIVE_PLAYERS = grabdata_all_crawl_sites()
 
-FOUND_PLAYERS = []
+FOUND_TOP_PLAYERS = []
 for top_player in TOP_PLAYERS:
     if top_player in ACTIVE_PLAYERS:
-        FOUND_PLAYERS.append(top_player)
-        print('Player:', top_player, '- Site:', ACTIVE_PLAYERS[top_player])
-if not FOUND_PLAYERS:
+        FOUND_TOP_PLAYERS.append(top_player)
+if FOUND_TOP_PLAYERS:
+    print('Found Leaderboard Players')
+    print('-------------------------')
+for top_player in FOUND_TOP_PLAYERS:
+    print('Player:', top_player, '- Site:', ACTIVE_PLAYERS[top_player])
+print('\n')
+if not FOUND_TOP_PLAYERS:
     print('No Top Players Found')
+    print('\n')
+
+FOUND_FOLLOWED_PLAYERS = []
+for followed_player in FOLLOWED_PLAYERS_LIST:
+    if followed_player in ACTIVE_PLAYERS:
+        FOUND_FOLLOWED_PLAYERS.append(followed_player)
+if FOUND_FOLLOWED_PLAYERS:
+    print('Found Followed Players')
+    print('-------------------------')
+for followed_player in FOUND_FOLLOWED_PLAYERS:
+    print('Player:', followed_player, '- Site:', ACTIVE_PLAYERS[followed_player])
+print('\n')
+if not FOUND_FOLLOWED_PLAYERS:
+    print('No Followed Players Found')
+    print('\n')
