a936d762718a59cdc29fb7d2ce7a2f6c89e04043
0xRainy <35612912+0xRainy@users.noreply.github.com>
2019-06-05 19:44:26 -0700

    Moved Leaderboard functions into their own class, store leaderboard in file

--- crawlscaper.py
+++ crawlscaper.py
@@ -1,5 +1,10 @@
+""" This script functions as a sort of 'Twitch.tv' for DCSS online servers
+    allowing the user to be notified when a top 20, on one or more
+    selected leaderboard,is active and playing.
+    It provides a link to spectate these players.
+"""
 import time
-import os
+from pathlib import Path
 from selenium import webdriver
 from bs4 import BeautifulSoup
 
@@ -24,39 +29,42 @@ from bs4 import BeautifulSoup
 # [print(i) for i in a]
 
 
-def grab_data_longest_winning_streaks():
-    """Returns list of players Top Winning Sreaks from
-       http://crawl.akrasiac.org/scoring/streaks.html"""
+class Leaderboards():
+    """Handles choosing a leaderboard to download data from.
+       The filename parameter must include *.txt
+    """
+    def __init__(self, leaderboard_type='', filename=''):
+        self.leaderboard_type = leaderboard_type
+        self.filename = filename
 
-    url = "http://crawl.akrasiac.org/scoring/streaks.html"
-    browser = webdriver.PhantomJS()
-    browser.get(url)
-    time.sleep(1)
-    soup = BeautifulSoup(browser.page_source, "lxml")
-    browser.quit()
-    table = soup.findChildren('tr')
-    user_list = ['test', 'test2']
-    for child in table:
-        child = child.findNext('a')
-        child = str(child)
-        user = child.split('>')[1]
-        user = user.split('<')[0]
-        user = user.lower()
-        if user in user_list:
-            pass
-        elif len(user_list) < 20:
-            user_list.append(user)
-        else:
-            pass
-    return user_list
-
-
-# Output super long userlist to txt file for checking results of #
-# grab_data_longest_winning_streaks()                            #
-##################################################################
-# with open('TopStreakPlayers.txt', 'w') as f:
-#     for i in grab_data_longest_winning_streaks():
-#         f.write(i + '\n')
+    def grab_leaderboard_data(self):
+        """Downloads leaderbord data from leaderboard_type (url) and outputs
+           formatted list to file separated by newline."""
+        filename = self.filename
+        url = self.leaderboard_type
+        browser = webdriver.PhantomJS()
+        browser.get(url)
+        soup = None
+        while soup is None:
+            soup = BeautifulSoup(browser.page_source, "lxml")
+            browser.quit()
+        table = soup.findChildren('tr')
+        leaderboard_list = ['test', 'test2']
+        for child in table:
+            child = child.findNext('a')
+            child = str(child)
+            user = child.split('>')[1]
+            user = user.split('<')[0]
+            user = user.lower()
+            if user in leaderboard_list:
+                pass
+            elif len(leaderboard_list) < 20:
+                leaderboard_list.append(user)
+            else:
+                pass
+        with open(filename, "w") as leaderfile:
+            for i in leaderboard_list:
+                leaderfile.write(i + '\n')
 
 
 def grabdata_all_crawl_sites():
@@ -65,7 +73,8 @@ def grabdata_all_crawl_sites():
     url = "https://crawl.develz.org/watch.htm"
     browser = webdriver.PhantomJS()
     browser.get(url)
-    time.sleep(1)
+    time.sleep(1)  # Maybe can do, While soup is false (empty), then when true,
+# continue?
     soup = BeautifulSoup(browser.page_source, "lxml")
     browser.quit()
     table = soup.findChildren('tr')
@@ -80,20 +89,28 @@ def grabdata_all_crawl_sites():
         else:
             user = child.split('-')[-1]
         user = user.split('"')[0]
-        # user_dict.update({user: site})
         user_dict[user] = site
     return user_dict
 
 
-TOP_PLAYERS = grab_data_longest_winning_streaks()
+LEADERBOARDS = Leaderboards('http://crawl.akrasiac.org/scoring/streaks.html',
+                            'top_streak.txt')
+LEADERBOARDS_FILE = Path('./' + LEADERBOARDS.filename)
+if LEADERBOARDS_FILE.is_file():
+    print('Leaderbord file found.')
+else:
+    print('No Leaderboard file found, creating one...')
+    Leaderboards.grab_leaderboard_data(LEADERBOARDS)
+
+with open(LEADERBOARDS.filename, "r") as afile:
+    TEMP_TOP_PLAYERS = afile.read()
+TOP_PLAYERS = (TEMP_TOP_PLAYERS.rstrip('\n')).splitlines()
 ACTIVE_PLAYERS = grabdata_all_crawl_sites()
 
 FOUND_PLAYERS = []
 for top_player in TOP_PLAYERS:
     if top_player in ACTIVE_PLAYERS:
         FOUND_PLAYERS.append(top_player)
+        print('Player:', top_player, '- Site:', ACTIVE_PLAYERS[top_player])
 if not FOUND_PLAYERS:
     print('No Top Players Found')
-else:
-    for i in FOUND_PLAYERS:
-        print(i, ACTIVE_PLAYERS[i])
