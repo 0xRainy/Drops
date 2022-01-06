0e82e9823c4dec9d21beef819ee99e53452ee9c0
0xRainy <35612912+0xRainy@users.noreply.github.com>
2022-01-06 13:41:55 -0700

    Changed input storage from Vec<char> to Vec<(char, u32)>

--- rust/compress_and_encode/src/main.rs
+++ rust/compress_and_encode/src/main.rs
@@ -10,45 +10,53 @@ fn main() {
         .expect("Failed to read line");
     // remove the newline character and carriage return
     input = input.replace(" ","").to_string();
+//TODO: Make compressed store tuples of (char, count)
     let mut compressed = Vec::new();
-    let mut count = 1;
+    let mut count: u32 = 1;
     let mut prev = input.chars().next().unwrap();
+    let mut pairs: (char, u32);
     for c in input.chars().skip(1) {
         if c.eq(&prev) {
             count += 1;
         } else if count == 1 {
-            compressed.push(prev);
+            pairs = (prev, count);
+            compressed.push(pairs);
             count = 1;
             prev = c;
         } else {
-            compressed.push(prev);
-            compressed.push(count as u8 as char);
+            pairs = (prev, count);
+            compressed.push(pairs);
             count = 1;
             prev = c;
         }
     }
-    compressed.push(prev);
-    compressed.push(count as u8 as char);
-    if compressed.last().unwrap() == &'1' {
+    pairs = (prev, count);
+    compressed.push(pairs);
+    if compressed.last().unwrap() == &(prev, 1) {
         compressed.pop();
     }
-    let encoded = compressed.into_iter().collect::<String>();
-    println!("{}", &encoded);
-    println!("{}", encode(encoded));
+    let encoded = &compressed;
+    let encoded_string = &compressed.iter().map(|(c, n)| format!("{}{}", c, n)).collect::<Vec<String>>().join("").replace("1","");
+    println!("{:?}", &encoded);
+    println!("{}", encode(encoded_string));
+    println!("{}", &encoded_string);
+    //println!("{}", decode_string(&encoded));
 }
 
 // For decoding the string, we need to reverse the process of encoding
 // First decode the string to a vector of bytes, then split the vector into a vector of strings
 // where each string is a character and a count, then reverse the process of encoding
 // to get the original string
-fn decode_string(input: &str) -> Vec<u8> {
-    let mut decoded = Vec::new();
-    let mut input = decode(input).unwrap();
-    while input.len() > 0 {
-        let c = input.pop().unwrap();
-        let count = input.pop().unwrap();
+fn decode_string(input: &Vec<char>) -> String {
+    let mut decoded = String::new();
+    let mut input = decode(&input.into_iter().collect::<String>()).unwrap();
+    for i in 0..input.len() {
+        let c = input[i];
+        let count = input[i + 1];
+        input.remove(i);
+        input.remove(i + 1);
         for _ in 0..count {
-            decoded.push(c);
+            decoded.push(c as char);
         }
     }
     decoded
